815b96fbedfff6330920da16cb50bdea6f1fb3b7
Dimitar Dzhevizov <dimitardzhevizov88@gmail.com>
2023-07-29 15:03:59 +0100

    Update firebase.utils.js
    enabling firebase auth

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -12,13 +12,14 @@ const firebaseConfig = {
   projectId: "crwn-clothing-99da5",
   storageBucket: "crwn-clothing-99da5.appspot.com",
   messagingSenderId: "866578135388",
-  appId: "1:866578135388:web:f7efe9015dca29a894a999",d
+  appId: "1:866578135388:web:f7efe9015dca29a894a999",
   
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
 
 const provider = new GoogleAuthProvider();
+// we acquire the class and store it in a variable provider
 
 provider.setCustomParameters({
   prompt: 'select_account',
@@ -32,3 +33,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+//we create variable and anonymous function and we must export them
+//signInWithGooglePopup is anonymous function
+//next is to enable Google Aut sign in method in Firebase
+//After that we go to src/routes/sign-in/sign-in.component.jsx 
