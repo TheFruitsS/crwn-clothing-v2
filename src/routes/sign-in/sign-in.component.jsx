26fc46500928bf6ef4b4ed9623c72948bfbf34a2
Dimitar Dzhevizov <dimitardzhevizov88@gmail.com>
2023-07-29 17:34:20 +0100

    lesson7
    Auth continue creating db

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,14 +1,13 @@
-import {
-  signInWithGooglePopup,
-  createUserProfileDocument,
+import {signInWithGooglePopup,
+  createUserDocumentFromAuth //we importing this func from firebase.utils so we can get access to our database
 } from '../../utils/firebase/firebase.utils';
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const response = await signInWithGooglePopup();
-    createUserProfileDocument(response);
+    const {user} = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);// we add our func here in the sign in function
   };
-
+// after that in the inspector we can see we have an object, const {user} is an object which we pass to create user doc from this object user
   return (
     <div>
       <h1>Sign In Page</h1>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -6,6 +6,14 @@ import {
   GoogleAuthProvider,
 } from 'firebase/auth';
 
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc
+
+} from 'firebase/firestore'
+
 const firebaseConfig = {
   apiKey: "AIzaSyDeu_IKN4Ptc-g3thNinylj0QubAKmN40E",
   authDomain: "crwn-clothing-99da5.firebaseapp.com",
@@ -25,15 +33,33 @@ provider.setCustomParameters({
   prompt: 'select_account',
 });
 
-export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
-
-  console.log(userAuth);
-};
-
 export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid)
+
+  console.log(userDocRef);
+
+  //next we create a snapshot so we can check our database if we have this object or not
+  //this snapshot create the same identifier in the memory which we can use to compare if we have it already
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+};
+
+
 //we create variable and anonymous function and we must export them
 //signInWithGooglePopup is anonymous function
 //next is to enable Google Aut sign in method in Firebase
 //After that we go to src/routes/sign-in/sign-in.component.jsx 
+//create database in firestore  in production mode => rules in rules edit write:if true; and save
+// after that we have to import methods getFirestore,doc,getDoc,setDoc
+//next we create a db and we can instantiate it
+//don't forget after acquiring a export const db '=' and the object
+
+//we can see in the inspection in the browser -> console in our app the output so we can prepare our database.We have to notice a stsTokenManager ->uid
+
+//we dont have users in our database or userAuth.uid in it,however google will create it for us
